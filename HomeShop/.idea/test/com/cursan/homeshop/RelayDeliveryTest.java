9140202881e014b457e0c55765f4c17900b2ff83
emaillot <emeline.maillot@thalesaleniaspace.com>
2021-05-04 18:51:40 +0200

    Modif des imports, bug persistant

--- HomeShop/.idea/test/com/cursan/homeshop/RelayDeliveryTest.java
+++ HomeShop/.idea/test/com/cursan/homeshop/RelayDeliveryTest.java
@@ -1,8 +1,7 @@
 package com.cursan.homeshop;
 
-import org.junit.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RelayDeliveryTest {
 
