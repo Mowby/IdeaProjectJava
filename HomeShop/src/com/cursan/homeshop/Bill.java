0eb60da2685c31b0f941142c4a220dfecfcd1051
emaillot <emeline.maillot@thalesaleniaspace.com>
2021-05-04 18:20:25 +0200

    Ajout attribut Delivery

--- HomeShop/src/com/cursan/homeshop/Bill.java
+++ HomeShop/src/com/cursan/homeshop/Bill.java
@@ -3,17 +3,18 @@ package com.cursan.homeshop;
 import java.util.Map;
 
 public class Bill {
-    Customer customer;
-    Map<Product, Integer> products;
-
+    private Customer customer;
+    private Map<Product, Integer> products;
+    private Delivery delivery;
     /**
      * Add a product with a quantity in the bill
      * @param product product to add
      * @param quantity quantity of the product
      */
 
-    public Bill(Customer customer){
+    public Bill(Customer customer, Delivery delivery){
         this.customer = customer;
+        this.delivery = delivery;
     }
 
     public void addProduct(Product product, Integer quantity){
